106323665557bd602b59bd65201e797f1c5a6b2b
sepxxx <you@example.com>
2023-03-05 11:37:18 +0300

    jinja modifications

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+# при написании render считаем что уже находимся в папке templates
 def index(request):
-    return HttpResponse("<h4>CHECK UP</h4>")
-
+    return render(request, 'main/index.html')
 def about(request):
-    return HttpResponse("<h4>ABOUT US</h4>")
+    return render(request, 'main/about.html')
