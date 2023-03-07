ffcf69b2260c644e1e6042a5f625c6a7e4ee1f48
sepxxx nb <itsmyoneforb@gmail.com>
2023-03-07 17:17:42 +0300

    news bd added

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Name', max_length=50)
+    anons = models.CharField('Anons', max_length=250)
+    full_text = models.TextField("Article")
+    date = models.DateTimeField('Pub date')
+    def __str__(self):
+        return self.title
+    class Meta:
+            verbose_name = "Article"
+            verbose_name_plural = "Articles"
+
+
