6a90690ade53ebb71f16d68f180a4c51831a5cf3
sepxxx nb <itsmyoneforb@gmail.com>
2023-03-08 13:11:03 +0300

    article delete functionality

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, DeleteView
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
@@ -32,3 +32,8 @@ class NewsUpdateView(UpdateView):
     template_name = 'news/create.html'
     # fields = ['title', 'anons', 'full_text', 'date']
     form_class = ArticlesForm
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = 'news/news_delete.html'
+    success_url = '/news'
+
